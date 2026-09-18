70c8c9b29cee1ae4f6fa1165b11b66817c99db4a
imranahmedkawser <imranahmedkawser@gmail.com>
2026-09-18 22:34:58 +0600

    Add counter animation functionality

---
+++ script.js
@@ -0,0 +1,35 @@
+// Counter Animation
+
+function animateCounter(id, target) {
+
+let count = 0;
+
+const element = document.getElementById(id);
+
+const speed = target / 100;
+
+const interval = setInterval(() => {
+
+count += speed;
+
+if (count >= target) {
+
+count = target;
+
+clearInterval(interval);
+
+}
+
+element.innerText = Math.floor(count) + "+";
+
+}, 20);
+
+}
+
+// Default Values
+
+animateCounter("donorCount", 500);
+
+animateCounter("donationCount", 1000);
+
+animateCounter("memberCount", 200);
